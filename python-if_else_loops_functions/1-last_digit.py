ab32d726ea2c19b2e99c669137ef3f2b4532ad60
Lionel DJANGUE <9183@holbertonstudents.com>
2024-07-17 20:21:43 +0000

    Deterline the last digit of a number

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+lastD = 0
+if number<0:
+   lastD=-number%10
+else:
+   lastD=number%10
+if lastD>5:  
+   print('Last digit of', number, 'is', lastD, 'and is greater than 5')
+elif lastD==0:  
+   print('Last digit of', number, 'is', lastD, 'and is 0')
+if number%10<6 and lastD != 0:  
+   print('Last digit of', number, 'is', lastD, 'and is less than 6 and not 0')
