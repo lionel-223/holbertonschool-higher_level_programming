f16b48403384f9bc2a17545812a470dde2e47106
Lionel DJANGUE <9183@holbertonstudents.com>
2024-07-18 11:11:32 +0000

    2 digit number

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i < 99:
-        print("%02d" % (i,), '', end='')
+        print("{}, ".format("%02d" % (i,)), end='')
     else:
-        print("%02d" % (i,))
+        print("{}".format("%02d" % (i,)))
