7248d98f81366b547823fd6ec4119e01131cdade
Lionel DJANGUE <9183@holbertonstudents.com>
2024-07-19 13:49:45 +0000

    FizzBuzz

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,12 +4,12 @@ def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 != 0:
             multi = "Fizz"
-            print("{} ".format(multi), end = "")
+            print("{} ".format(multi), end="")
         elif i % 5 == 0 and i % 3 != 0:
             multi = "Buzz"
-            print("{} ".format(multi), end = "")
+            print("{} ".format(multi), end="")
         elif i % 5 == 0 and i % 3 == 0:
             multi = "FizzBuzz"
-            print("{} ".format(multi), end = "")
+            print("{} ".format(multi), end="")
         else:
-            print("{} ".format(i), end = "")
+            print("{} ".format(i), end="")
