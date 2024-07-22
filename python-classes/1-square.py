3a6654265e8f67f0137071834542e72819e71427
Lionel DJANGUE <9183@holbertonstudents.com>
2024-07-22 13:19:53 +0000

    Square class

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A Square class for practicing object-oriented programming."""
+
+
+class Square:
+    """The size of the square (private attribute)."""
+    def __init__(self, size):
+        self.__size = size    
