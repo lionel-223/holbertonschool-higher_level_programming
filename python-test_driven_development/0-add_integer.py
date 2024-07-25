21de1d377a95032dd53414ffe4649e990e6331a9
Lionel DJANGUE <lionelmonthe184@gmail.com>
2024-07-25 12:11:05 +0000

    0. Integers addition

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+This module supplies one function, add_integer(). For example,
+
+>>> add_integer(1, 2)
+3
+>>> add_integer(100, -2)
+98
+>>> add_integer(2)
+100
+>>> add_integer(100.3, -2)
+98
+>>> add_integer(4, "School")
+Traceback (most recent call last):
+    ...
+TypeError: b must be an integer
+>>> add_integer(None)
+Traceback (most recent call last):
+    ...
+TypeError: a must be an integer
+"""
+
+def add_integer(a, b=98):
+    """
+    Adds two integers or floats (after casting them to integers).
+
+    Arguments:
+    a -- the first number
+    b -- the second number (default 98)
+
+    Returns:
+    The integer sum of a and b.
+
+    Raises:
+    TypeError -- if a or b is not an integer or float.
+    """
+    if not isinstance(a, (int, float)):
+         raise TypeError("a must be and integer")
+    if not isinstance(b, (int, float)):
+         raise TypeError("b must be and integer")
+    return int(a)+int(b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile()
