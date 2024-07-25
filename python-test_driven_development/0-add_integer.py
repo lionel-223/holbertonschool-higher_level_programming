c446e99ba95c7d81c198e851993bc21836e28fd8
Lionel DJANGUE <lionelmonthe184@gmail.com>
2024-07-25 14:28:47 +0000

    0. Integers addition

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -20,6 +20,7 @@ Traceback (most recent call last):
 TypeError: a must be an integer
 """
 
+
 def add_integer(a, b=98):
     """
     Adds two integers or floats (after casting them to integers).
@@ -35,7 +36,7 @@ def add_integer(a, b=98):
     TypeError -- if a or b is not an integer or float.
     """
     if not isinstance(a, (int, float)):
-         raise TypeError("a must be an integer")
+        raise TypeError("a must be an integer")
     elif not isinstance(b, (int, float)):
-         raise TypeError("b must be an integer")
+        raise TypeError("b must be an integer")
     return int(a)+int(b)
