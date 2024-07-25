771bc8f79c577747a43b2988ef119aa1dd82ab14
Lionel DJANGUE <lionelmonthe184@gmail.com>
2024-07-25 14:05:27 +0000

    0. Integers addition

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -36,10 +36,6 @@ def add_integer(a, b=98):
     """
     if not isinstance(a, (int, float)):
          raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    elif not isinstance(b, (int, float)):
          raise TypeError("b must be an integer")
     return int(a)+int(b)
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile()
